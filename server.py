d996a607e16c9716bdfd192c91f63ec861e0f411
Tomas <prologas@protonmail.com>
2020-03-18 19:34:49 +0200

    User adenaCount check 6

--- server.py
+++ server.py
@@ -77,6 +77,8 @@ class sellAdena(Resource):
 
         if onlineStatus[0]['online'] != 0:
             return jsonify(data=loggedin)
+        elif int(adenaCountStatus[0]['count']) >= 1000: # Check if user have enough adena to sell
+            return jsonify(data=adenaFail)
         elif int(adenaCountStatus[0]['count']) < int(count): # Check if user have enough adena to sell
             return jsonify(data=adenaFail)
         elif account == '':
