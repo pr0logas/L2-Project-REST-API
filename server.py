6438d4444bed10cb36e429c7d59ce947022cd04d
Tomas <prologas@protonmail.com>
2020-03-18 17:17:36 +0200

    User registration check 3

--- server.py
+++ server.py
@@ -71,12 +71,30 @@ class adenaCount(Resource):
 # http://127.0.0.1:9005/apiv1/register?user=test&passw=test&mail=info@adeptio.cc
 class register(Resource):
     def get(self):
+        check = False
         fail = json.loads('{"ERROR" : "Invalid username/password or email. Please check your data"}')
         user = str(request.args.get('user'))
         passw = str(request.args.get('passw'))
-        mail = str(request.args.get('mail'))
+        mail = str(request.args.get('email'))
+        regex = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
         hashBase64 = base64.b64encode(hashlib.sha1(passw.encode('utf8')).digest())
-        if user != '' and passw != '' and mail != '' and mail != None and user != None:
+
+        # Not null inputs
+        if user == '' or passw == '' or mail == '' or user == None or passw == None or mail == None:
+            print('Invalid input > check failed')
+            check = False
+        else
+            check = True
+
+        # Valid email?
+        if (re.search(regex, mail)):
+            check = True
+        else:
+            print('Invalid email > check failed')
+            check = False
+
+        # Query start
+        if check == True:
             cursorLG.execute("insert into accounts (login, password, email) values (%s, %s, %s);", (user, hashBase64, mail))
             return jsonify(data=cursorLG.fetchall())
         else:
