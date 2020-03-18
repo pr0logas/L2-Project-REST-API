702755cdc994abe1d36058fe4210ff339561ce59
Tomas <prologas@protonmail.com>
2020-03-18 17:55:54 +0200

    User registration check 5

--- server.py
+++ server.py
@@ -25,7 +25,7 @@ def get_real_ip():
 # Flask rules
 app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app, num_proxies=1)
-limiter = Limiter(app, key_func=get_real_ip, default_limits=["60/minute"])
+limiter = Limiter(app, key_func=get_real_ip, default_limits=["10/minute"])
 app.url_map.strict_slashes = False
 api = Api(app, prefix="/apiv1")
 
@@ -46,7 +46,7 @@ class getInfo(Resource):
 # http://127.0.0.1:9005/apiv1/getInfo?account=adeptio
 class getUserInfo(Resource):
     def get(self):
-        userAcc = request.args.get('account')
+        userAcc = request.args.get
         cursor.execute("select char_name,account_name,onlinetime,pvpkills,charId,`level` from characters WHERE account_name=%s;", userAcc)
         cursor.close()
         return jsonify(data=cursor.fetchall())
@@ -54,7 +54,7 @@ class getUserInfo(Resource):
 # http://127.0.0.1:9005/apiv1/getMoneyCount?charId=268481220
 class getMoneyCount(Resource):
     def get(self):
-        userCharId = int(request.args.get('charId'))
+        userCharId = int(request.args.get)
         cursor.execute("select count from items WHERE item_id=57 and owner_id=%s;", userCharId)
         cursor.close()
         return jsonify(data=cursor.fetchall())
@@ -62,9 +62,9 @@ class getMoneyCount(Resource):
 # http://127.0.0.1:9005/apiv1/adenaCount?owner=268481220&count=1234&token=540215452
 class adenaCount(Resource):
     def get(self):
-        owner_id = str(request.args.get('owner'))
-        count = int(request.args.get('count'))
-        token = int(request.args.get('token'))
+        owner_id = str(request.args.get)
+        count = int(request.args.get)
+        token = int(request.args.get)
         if token != None:
             cursor.execute("update items set count=%s WHERE item_id=57 and owner_id=%s;", (count, owner_id))
             cursor.close()
@@ -74,16 +74,18 @@ class adenaCount(Resource):
 
 # http://127.0.0.1:9005/apiv1/register?user=test&passw=test&mail=info@adeptio.cc
 class register(Resource):
+
     def get(self):
-        check = False
         already = json.loads('{"ERROR" : "User already exists?"}')
         fail = json.loads('{"ERROR" : "Invalid username/password or email. Please check your data"}')
         success = json.loads('{"SUCCESS" : "Registration successful"}')
-        user = str(request.args.get('user'))
-        passw = str(request.args.get('passw'))
-        mail = str(request.args.get('email'))
+        user = str(request.args.get)
+        passw = str(request.args.get)
+        mail = str(request.args.get)
         regex = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
         hashBase64 = base64.b64encode(hashlib.sha1(passw.encode('utf8')).digest())
+
+        check = False
 
         # Not null inputs
         if user == '' or passw == '' or mail == '' or user == None or passw == None or mail == None:
@@ -102,8 +104,7 @@ class register(Resource):
         # Query start
         if check == True:
             try:
-                res = cursorLG.execute("insert into accounts (login, password, email) values (%s, %s, %s);", (user, hashBase64, mail))
-                print(res)
+                cursorLG.execute("insert into accounts (login, password, email) values (%s, %s, %s);", (user, hashBase64, mail))
                 return jsonify(data=success)
             except:
                 return jsonify(data=already)
