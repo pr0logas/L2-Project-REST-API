31ecd93cf2c9e6e97160e925e0a8d6b37cdd23ce
Tomas <prologas@protonmail.com>
2020-03-19 17:09:04 +0200

    Debug

--- server.py
+++ server.py
@@ -143,7 +143,9 @@ class sellAdena(Resource):
 
         if onlineStatus[0]['online'] != 0:
             return jsonify(data=loggedin)
-        elif int(adenaCountStatus[0]['count']) <= 1000 and count >= 1000: # Check if user have enough adena to sell
+        elif int(adenaCountStatus[0]['count']) <= 1000: # Check if user have enough adena to sell
+            return jsonify(data=adenaFail2)
+        elif int(count) >= 1000:
             return jsonify(data=adenaFail2)
         elif int(adenaCountStatus[0]['count']) < int(count): # Check if user have enough adena to sell
             return jsonify(data=adenaFail)
