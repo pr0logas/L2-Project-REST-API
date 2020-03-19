f70cb262f0cf16a2bcce67bcf9a0be4791ca1c43
Tomas <prologas@protonmail.com>
2020-03-19 17:10:33 +0200

    Debug

--- server.py
+++ server.py
@@ -140,6 +140,7 @@ class sellAdena(Resource):
         cursor.execute("select count from items WHERE item_id=57 and owner_id=%s;", owner_id)
         adenaCountStatus = cursor.fetchall()
 
+        print(int(count))
 
         if onlineStatus[0]['online'] != 0:
             return jsonify(data=loggedin)
