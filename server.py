f521f410e521bb2877cbe93ae9807eb0c9ea259d
Tomas <prologas@protonmail.com>
2020-03-19 20:02:51 +0200

    deposit Adeptio

--- server.py
+++ server.py
@@ -236,7 +236,7 @@ class depositAdeptio(Resource):
             command = 'adeptio-cli -rpcconnect=' + host + ' -rpcuser=' + user + ' -rpcpassword=' + passwd  + ' -rpcclienttimeout=' + timeout + ' getnewaddress'
             result = subprocess.check_output(command,shell=True).strip()
             result = result.decode("utf-8")
-            onlyWlt = result['data']
+            onlyWlt = result[0]['data']
             cursorLG.execute("replace into adeptio_balances values lastdepositwlt=%s WHERE login=%s;", (onlyWlt, account))
             return jsonify(data=result)
         else:
