3ac51d659b312a32e55368b3d020d44ea9dafe06
Tomas <prologas@protonmail.com>
2020-03-22 22:47:11 +0200

    Get Crypto prices method debug

--- server.py
+++ server.py
@@ -341,7 +341,7 @@ class getCryptoPrices(Resource):
         response = urllib.request.urlopen(req)
 
         try:
-            restmp = response.read().decode('utf-8').replace('\n', '')
+            restmp = response.read().decode('utf-8')
             print(restmp)
             aa = json.dumps(restmp)
             print(aa)
