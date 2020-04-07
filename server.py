e32a5874c655c11959d84f6d54cf305300cd6352
Tomas <prologas@protonmail.com>
2020-04-08 00:12:15 +0300

    GetUser info with clan name

--- server.py
+++ server.py
@@ -63,7 +63,8 @@ def checkMail(value):
 class getInfo(Resource):
     def get(self):
         cursor = createCursor()
-        cursor.execute("SELECT char_name,account_name,onlinetime,pvpkills,charId,`level`,classid,clan_data.clan_name FROM characters"
+        cursor.execute("SELECT characters.char_name, characters.account_name, characters.onlinetime, characters.pvpkills, "
+                       "characters.charId, characters.level characters.classid, clan_data.clan_name FROM characters "
                        "INNER JOIN clan_data ON characters.clanid=clan_data.clan_id")
         cursor.close()
         return jsonify(data=cursor.fetchall())
