fca3af2c5591c75ff2e004cc0f44f6f1ff01d77a
Tomas <prologas@protonmail.com>
2020-05-04 20:52:53 +0300

    Activate ref code

--- server.py
+++ server.py
@@ -79,6 +79,8 @@ class getWealth(Resource):
         cursor.close()
         return jsonify(data=cursor.fetchall())
 
+### Without REFERAL link;
+'''
 # http://127.0.0.1:9005/apiv1/register?user=test&passw=test&email=info@ababas.lt
 class register(Resource):
 
@@ -110,6 +112,69 @@ class register(Resource):
                 try:
                     cursorLG.execute("insert into accounts (login, password, email) values (%s, %s, %s);", (user, hashBase64, mail))
                     cursorLG.close()
+                    return jsonify(data=success)
+                except:
+                    cursorLG.close()
+                    return jsonify(data=already)
+            else:
+                print("Failed mail check")
+                print('email: ', mail)
+                cursorLG.close()
+                return jsonify(data=fail)
+        else:
+            print("Failed username/password check")
+            print('user/passw: ', user, passw)
+            cursorLG.close()
+            return jsonify(data=fail)
+'''
+### To create REF -> openssl rand -base64 3
+# http://127.0.0.1:9005/apiv1/register?user=test&passw=test&email=info@ababas.lt&ref=aBc1
+class register(Resource):
+
+    def get(self):
+        cursorLG = createCursorLG()
+        already = json.loads('{"ERROR" : "User already exists?"}')
+        alreadyMail = json.loads('{"ERROR" : "Email already exists?"}')
+        alreadyRef = json.loads('{"ERROR" : "Referral code does not exist?"}')
+        fail = json.loads('{"ERROR" : "Invalid username/password or email. Please check your data"}')
+        success = json.loads('{"SUCCESS" : "Registration successful. Now you can start a game. Collect some adena first and login to your control panel"}')
+        user = str(request.args.get('user'))
+        passw = str(request.args.get('passw'))
+        mail = str(request.args.get('email'))
+        referral = str(request.args.get('ref'))
+        regex = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
+        hashBase64 = base64.b64encode(hashlib.sha1(passw.encode('utf8')).digest())
+
+        # Check if ref exists?
+        cursorLG.execute("SELECT code FROM referral_code WHERE code=%s;", referral)
+        checkRef = cursorLG.fetchall()
+
+        try:
+            if str(checkRef[0]['code']) == referral:
+                pass
+        except:
+            return jsonify(data=alreadyRef)
+
+        # Check if mail exists?
+        cursorLG.execute("SELECT email FROM accounts WHERE email=%s;", mail)
+        checkMail = cursorLG.fetchall()
+
+        try:
+            if str(checkMail[0]['email']) == mail:
+                return jsonify(data=alreadyMail)
+        except:
+            pass
+
+        # Query start
+        if user != '' and passw != '':
+            if (re.search(regex, mail)):
+                try:
+                    cursorLG.execute("insert into accounts (login, password, email) values (%s, %s, %s);", (user, hashBase64, mail))
+                    cursorLG.close()
+
+                    cursorLG.execute("DELETE FROM referral_code WHERE code=%s;", (referral))
+                    cursorLG.close()
+
                     return jsonify(data=success)
                 except:
                     cursorLG.close()
