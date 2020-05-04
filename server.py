c949e0c1e2aa9bedbddabfd479b6a9f1035b53d8
Tomas <prologas@protonmail.com>
2020-05-04 20:13:01 +0300

    Preregistration method

--- server.py
+++ server.py
@@ -80,6 +80,91 @@ class getWealth(Resource):
         cursor.close()
         return jsonify(data=cursor.fetchall())
 
+# http://127.0.0.1:9005/apiv1/register?user=test&passw=test&email=info@ababas.lt
+class register(Resource):
+
+    def get(self):
+        cursorLG = createCursorLG()
+        already = json.loads('{"ERROR" : "User already exists?"}')
+        alreadyMail = json.loads('{"ERROR" : "Email already exists?"}')
+        fail = json.loads('{"ERROR" : "Invalid username/password or email. Please check your data"}')
+        success = json.loads('{"SUCCESS" : "Registration successful. Now you can start a game. Collect some adena first and login to your control panel"}')
+        user = str(request.args.get('user'))
+        passw = str(request.args.get('passw'))
+        mail = str(request.args.get('email'))
+        regex = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
+        hashBase64 = base64.b64encode(hashlib.sha1(passw.encode('utf8')).digest())
+
+        # Check if mail exists?
+        cursorLG.execute("SELECT email FROM accounts WHERE email=%s;", mail)
+        checkMail = cursorLG.fetchall()
+
+        try:
+            if str(checkMail[0]['email']) == mail:
+                return jsonify(data=alreadyMail)
+        except:
+            pass
+
+        # Query start
+        if user != '' and passw != '':
+            if (re.search(regex, mail)):
+                try:
+                    cursorLG.execute("insert into accounts (login, password, email) values (%s, %s, %s);", (user, hashBase64, mail))
+                    cursorLG.close()
+                    return jsonify(data=success)
+                except:
+                    cursorLG.close()
+                    return jsonify(data=already)
+            else:
+                print("Failed mail check")
+                print('email: ', mail)
+                cursorLG.close()
+                return jsonify(data=fail)
+        else:
+            print("Failed username/password check")
+            print('user/passw: ', user, passw)
+            cursorLG.close()
+            return jsonify(data=fail)
+
+
+# http://127.0.0.1:9005/apiv1/preregister?&email=info@ababas.lt
+class preregister(Resource):
+
+    def get(self):
+        cursorLG = createCursorLG()
+
+        ip = (str(request.remote_addr))
+        fail = json.loads('{"ERROR" : "Invalid email. Please check your data"}')
+        alreadyMail = json.loads('{"ERROR" : "Email already exists?"}')
+        success = json.loads('{"SUCCESS" : "Notify successful."}')
+        mail = str(request.args.get('email'))
+        regex = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
+
+        # Check if mail exists?
+        cursorLG.execute("SELECT email FROM pre_registration WHERE email=%s;", mail)
+        checkMail = cursorLG.fetchall()
+
+        try:
+            if str(checkMail[0]['email']) == mail:
+                return jsonify(data=alreadyMail)
+        except:
+            pass
+
+        # Query start
+        if (re.search(regex, mail)):
+            try:
+                cursorLG.execute("insert into pre_registration (email, lastIP) values (%s, %s);", (email, ip))
+                cursorLG.close()
+                return jsonify(data=success)
+            except:
+                cursorLG.close()
+                return jsonify(data=already)
+        else:
+            print("Failed mail check")
+            print('email: ', mail)
+            cursorLG.close()
+            return jsonify(data=fail)
+
 
 # http://127.0.0.1:9005/apiv1/getClans?
 class getClans(Resource):
@@ -606,6 +691,7 @@ api.add_resource(getAdeptioUserInfo, '/getAdeptioUserInfo')
 api.add_resource(getMoneyCount, '/getMoneyCount')
 api.add_resource(getUserMoneyCount, '/getUserMoneyCount')
 api.add_resource(register, '/register')
+api.add_resource(preregister, '/preregister')
 api.add_resource(sellAdena, '/sellAdena')
 api.add_resource(buyAdena, '/buyAdena')
 api.add_resource(depositAdeptio, '/depositAdeptio')
